67df4c8a7d8de5d4cd5489197b79ef3f9c8d1bb3
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-09-23 13:54:12 +0000

    changed license

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 const { CediPlus } = require('./services/charge.wallet');
-
-
-const wallet = new CediPlus(API_KEY, DESCRIPTION, WALLET_NUMBER, WALLET_TYPE, AMOUNT, PRODUCTION_CALLBACK_URL);
-
-wallet.Charge()
-  .then((response) => {
-    return console.log(response);
-  })
-  .catch((error) => {
-    return console.log(error)
-  });
